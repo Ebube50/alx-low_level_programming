e1791c06f8c9136fdc337d088c24e3d67d67879c
Ebube50 <ebubennaemeka@gmail.com>
2022-12-10 05:42:10 -0800

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+/**
+ * printf function - to print text
+ */
 int main(void)
-/*Printf function to print text*/
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
