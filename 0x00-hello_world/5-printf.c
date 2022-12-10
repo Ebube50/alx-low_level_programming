b320d08f905c83db56302f4fffc4fffe2650cb60
Ebube50 <ebubennaemeka@gmail.com>
2022-12-10 06:02:37 -0800

    hello print file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - to print text
- * return: 0 success
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 	/*this function print to the stdout*/
