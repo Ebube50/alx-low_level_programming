fc896f4122d34c2dbd2d6e697200c4b11e6ca862
Ebube50 <ebubennaemeka@gmail.com>
2022-12-16 09:03:50 +0000

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+    
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d\n", n);
+	return (0);
+}
