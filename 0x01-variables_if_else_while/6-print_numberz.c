c53dfdbdbbf1ef36bbc4f1e4739da11718d2798d
Ebube50 <ebubennaemeka@gmail.com>
2022-12-17 16:47:33 +0000

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- * Main - Entry point - print number 0-9
+ * main - Entry point - print number 0-9
  * Return: 0 (success)
  */
 int main(void)
 {
 	int i;
-	
 	for (i = 48; i <= 57; i++)
-		{
-			putchar(i);
-		}
+	{
+		putchar(i);
+	}
 	putchar(10);
 	return (0);
 }
