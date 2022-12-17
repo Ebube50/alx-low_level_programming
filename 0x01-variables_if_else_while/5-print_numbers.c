9cab034c8dd8c4bdfde98016e0387be4b1d8f796
Ebube50 <ebubennaemeka@gmail.com>
2022-12-17 11:55:53 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* 
+ * main - entry point, print single digits 0 to 9
+ * Return: o (success)
+ */
+int main(void)
+{
+	int i=0;
+	while (i < 10)
+	{
+		printf("%d\n", i++);
+	}
+	return (0);
+}
