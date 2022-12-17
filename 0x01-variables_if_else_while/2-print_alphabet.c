c6eb2a935b0982e567eeb994157e6bbb313d3463
Ebube50 <ebubennaemeka@gmail.com>
2022-12-17 10:27:24 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char i;
+	i='a';
+	while(i<='z')
+	{
+		putchar(i++);
+	}
+	putchar(10);
+	return (0);
+}
