67bb4d1e281e9c0b7e0111b1f6bf89de8bfe7191
Ebube50 <ebubennaemeka@gmail.com>
2022-12-21 14:48:16 +0000

    task3 print int 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void print_numbers(void) print int 0 to 9
+ *
+ * Return 0 success
+ */
+void print_numbers(void)
+{
+	int i=0;
+
+	while (i < 10)
+	{
+		i++
+		return (i);
+	}
+}
