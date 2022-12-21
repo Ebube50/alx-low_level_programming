6613269d71bad570d70ea8d1394b5f0175d4c618
Ebube50 <ebubennaemeka@gmail.com>
2022-12-21 14:20:08 +0000

    task 2 function returns product of 2 nunbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * int mul(int a, int b) print the result of product of @a and @b
+ * Return: 0 success
+ */
+
+int mul(int a, int b);
+{
+	int result;
+
+	result = a*b;
+
+	return (result);
+}
